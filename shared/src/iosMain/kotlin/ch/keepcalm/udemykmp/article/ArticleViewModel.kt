87b261daa7273eba35eb57e07d8c67b8d18832a8
monkey <marzelwidmer@gmail.com>
2024-09-13 15:12:14 +0200

    Articles View Model part 2

--- shared/src/iosMain/kotlin/ch/keepcalm/udemykmp/article/ArticleViewModel.kt
+++ shared/src/iosMain/kotlin/ch/keepcalm/udemykmp/article/ArticleViewModel.kt
@@ -1,8 +1,10 @@
 package ch.keepcalm.udemykmp.article
 
 import ch.keepcalm.udemykmp.BaseViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 
 class ArticleViewModel() : BaseViewModel() {
@@ -10,5 +12,16 @@ class ArticleViewModel() : BaseViewModel() {
     private val _articlesState: MutableStateFlow<ArticleState> = MutableStateFlow(ArticleState())
 
     val articleState: StateFlow<ArticleState> get() = _articlesState
+
+    init {
+        getArticles()
+    }
+
+    private fun getArticles() {
+        scope.launch {
+            delay(500)
+            _articlesState.emit(ArticleState())
+        }
+    }
 
 }
